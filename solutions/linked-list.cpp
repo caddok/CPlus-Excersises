b291d3ecacc8692f17290060417a671b7d73bde0
Georgi Delchev <g.delchev93@gmail.com>
2017-12-02 13:16:39 +0200

    Added expection handling for insert

--- solutions/linked-list.cpp
+++ solutions/linked-list.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -10,12 +11,23 @@ class LinkedList
         node *next;        
     };
     private:
-    node *head,*tail,count;
+    node *head,*tail;
     public:
     LinkedList()
     {
         head = NULL;
         tail = NULL;
+    }
+    int count()
+    {
+        node *temp = head;
+        int length = 0;
+        while (temp != NULL)
+        {
+            length++;
+            temp = temp -> next;
+        }
+        return length;
     }
     void push (int value)
     {
@@ -24,15 +36,23 @@ class LinkedList
         temp->next = head;
         head = temp;
     }
-    void insert(int position,int value)
+    void insert(int value,int position)
     {
         node *temp = new node;
         node *current = new node;
         node *previous = new node;
-        for (size_t i = 1; i < position; i++)
+        runtime_error space("The list is too short");
+        if(position < LinkedList::count()) 
+        {    
+            for (size_t i = 1; i < position; i++)
+            {
+                previous = current;
+                current = current->next;
+            }
+        }
+        else
         {
-            previous = current;
-            current = current->next;
+            throw space;
         }
     }
 };
@@ -40,19 +60,19 @@ class LinkedList
 int main()
 {
     LinkedList* list = new LinkedList();
-    list.push(10);  // Adds new item at the begining of the list
+    list->push(10);  // Adds new item at the begining of the list
     int value = 20, position = 4;
-    list.insert(value, position); // inserts a value at a position inside the list, if the list is too short throws an exception;
-    list.peek(); // returns the first element of the list
-    list.pop(); // removes the first element of the list throws exception if no elements exist
-    list.erase(10) // removes the element at index 10, throws exception if index is out of range
-    list.elementAt(22) // returns the element at index 22, throws exception if index is out of range
-    list.size(); // returns the number of elements in the list
-    list.reverse(); // reverses the list
-    list.print(); // prints the list to stdout
+    list->insert(value, position); // inserts a value at a position inside the list, if the list is too short throws an exception;
+    // list.peek(); // returns the first element of the list
+    // list.pop(); // removes the first element of the list throws exception if no elements exist
+    // list.erase(10) // removes the element at index 10, throws exception if index is out of range
+    // list.elementAt(22) // returns the element at index 22, throws exception if index is out of range
+     list->count(); // returns the number of elements in the list
+    // list.reverse(); // reverses the list
+    // list.print(); // prints the list to stdout
 
-    int arr[] = {1, 5, 22, -7, 38};
-    LinkedList* list2 = new LinkedList(arr); //creates a list from array
+    // int arr[] = {1, 5, 22, -7, 38};
+    // LinkedList* list2 = new LinkedList(arr); //creates a list from array
 
 
     return 0;
